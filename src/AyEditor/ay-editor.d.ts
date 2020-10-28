389219ada6ed25bb220980b460822b81b1aacf35
viewweiwu <648764190@qq.com>
2020-10-28 19:58:23 +0800

    feat: 删除内置富文本编辑器

--- src/AyEditor/ay-editor.d.ts
+++
@@ -1,8 +0,0 @@
-export interface AyEditorProps {
-  /** 值 */
-  value?: any
-  /** change 事件监听 */
-  onChange?: (value: string) => void
-  disabled?: boolean
-  placeholder?: string
-}
--- src/AyEditor/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { AyEditorProps } from './ay-editor'
-
-declare const AyEditor: React.ForwardRefExoticComponent<AyEditorProps & React.RefAttributes<HTMLDivElement>>
-
-export default AyEditor
