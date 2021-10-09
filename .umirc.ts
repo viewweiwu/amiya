16715a91733ea5de43a15306f9f1e642f1cd14e0
viewweiwu <648764190@qq.com>
2021-10-09 17:42:51 +0800

    doc: 更改站点模式

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,30 @@ export default defineConfig({
   logo: '/amiya/images/logo.jpg',
   outputPath: 'docs-dist',
   publicPath: '/amiya/',
-  base: '/amiya/'
-  // more config: https://d.umijs.org/config
+  base: '/amiya/',
+  mode: 'site',
+  menus: {
+    // 需要自定义侧边菜单的路径，没有配置的路径还是会使用自动生成的配置
+    '/form': [
+      {
+        title: '菜单项',
+        path: '菜单路由（可选）',
+        children: ['/form/卡片表单.md']
+      }
+    ]
+  },
+  navs: [
+    {
+      title: '体验',
+      path: '/'
+    },
+    {
+      title: '组件',
+      path: '/ccomponents'
+    },
+    {
+      title: 'GitHub',
+      path: 'https://github.com/viewweiwu/amiya'
+    }
+  ]
 })
