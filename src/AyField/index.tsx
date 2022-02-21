92eef492c8f5c4f9c42a8331956fdb99f7b61c00
viewweiwu <648764190@qq.com>
2022-02-21 14:09:45 +0800

    fix: 修复错误的引用

--- src/AyField/index.tsx
+++ src/AyField/index.tsx
@@ -1,4 +1,4 @@
-import { AySearchTableField } from 'amiya'
+import { AySearchTableField } from '../AySearchTable/ay-search-table'
 import { AyFormField } from '../AyForm/ay-form'
 
 export default function AyField(_: AyFormField | AySearchTableField) {
